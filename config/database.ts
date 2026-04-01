272d91a0a70b337a516de201c97bfbd00f64e3b2
Spinach812 <thanhtiendo526@gmail.com>
2026-04-01 20:22:33 +0700

    Update - Show Task

---
+++ config/database.ts
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+export const connect = async (): Promise<void> => {
+  try {
+    await mongoose.connect(process.env.mongo_url);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
+};
---
+++ index.ts
@@ -0,0 +1,22 @@
+import express, { Express, Request, Response } from "express";
+import * as database from "./config/database";
+import dotenv from "dotenv";
+import { Task } from "./models/task.model";
+
+dotenv.config();
+
+const app: Express = express();
+const port: number | string = process.env.port || 3000;
+
+database.connect();
+
+app.get("/tasks", async (req: Request, res: Response) => {
+  const tasks = await Task.find({
+    deleted: false,
+  });
+  res.json(tasks);
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
